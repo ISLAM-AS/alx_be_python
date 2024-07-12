e07c5266145e3790ef7e9fd518210ab2b5070c64
Islam AS <is.1997@outlook.com>
2024-07-12 19:43:20 +0300

    add baisic_operations.py & run the codes

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#Assiging specific values for two variables, number1 and number2:
+number1 = 10
+number2 = 5
+
+#Performing addition, subtraction, and multiplication on these numbers:
+result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+#Print the results of these operations in a human-readable format:
+print("sum of", number1, "and", number2, "is", result)
+print("difference of", number1, "and", number2, "is", difference)
+print("product of", number1, "and", number2, "is", product)
