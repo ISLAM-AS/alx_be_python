df181f519c19583ffaab7453f57af2b76abfc4de
ISLAM <is.1997@outlook.com>
2024-08-20 02:09:59 +0300

    Added multiplication_table.py

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,12 @@
+## multiplication_table.py
+
+# Prompt User for a Number:
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and Print the Multiplication Table:
+for x in range(1, 11):
+    product = x * 5
+    print(f"{number} * {x}")
+
+
+
